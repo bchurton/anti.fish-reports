959e978113443e4fd55148795f87aecd78d4ee8c
bchurton <ben@churton.uk>
2022-06-22 00:27:07 +0100

    Add files via upload

---
+++ commands/report.js
@@ -0,0 +1,27 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+module.exports = {
+    name: "report",
+	usage: "report",
+	description: "Submit a domain report!",
+    async execute(client, message, args) {
+		let embed = new MessageEmbed()
+			.setDescription(`Want to report a domain? Click the button below to report a domain to Fishfish.gg!`)
+			.setColor("ffffff")
+		let row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setCustomId(`submitReport${message.author.id}`)
+				.setLabel('Report Domain')
+				.setStyle('SUCCESS'),
+		)
+		.addComponents(
+			new MessageButton()
+				.setCustomId(`cancelReport${message.author.id}`)
+				.setLabel('Dismiss')
+				.setStyle('DANGER'),
+		)
+		
+		const msg = await message.channel.send({ embeds:[embed], components:[row] })
+		
+	}
+}
