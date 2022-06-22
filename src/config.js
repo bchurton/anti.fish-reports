f77171f69955bdc7d18caed9ea7a00953614d0dd
T3NED <software@t3ned.com>
2022-06-22 02:09:51 +0100

    Refactor modal submit event

--- src/config.js
+++ src/config.js
@@ -2,10 +2,14 @@ const discordToken = process.env.DISCORD_TOKEN;
 const commandPrefix = process.env.COMMAND_PREFIX;
 const debug = process.env.DEBUG === "true";
 const repositoryUrl = process.env.REPOSITORY_URL;
+const submitReportDomain = process.env.SUBMIT_REPORT_DOMAIN;
+const submitReportAuthorization = process.env.SUBMIT_REPORT_AUTHORIZATION;
 
 module.exports = {
 	discordToken,
 	commandPrefix,
 	debug,
 	repositoryUrl,
+	submitReportDomain,
+	submitReportAuthorization,
 };
--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -1,32 +1,59 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
-const fetch = require("node-fetch");
+const { printError } = require("../utils/console");
+const config = require("../config");
+const axios = require("axios");
 
 module.exports = {
 	name: "modalSubmit",
-	async execute(client, modal) {
+	async execute(_client, modal) {
 		const domain = modal.getTextInputValue("domain");
 		const reason = modal.getSelectMenuValues("reason")[0];
 		const extra = modal.getTextInputValue("extra");
 
-		const response = await fetch("https://yuri.bots.lostluma.dev/phish/report", {
-			method: "POST",
-			body: JSON.stringify({
-				url: domain,
-				reason: `Reported by \`${modal.user.username}#${modal.user.discriminator}\` via the Anti.fish API server.\nReport reason: ${reason}\nAdditional details: ${extra}`,
-			}),
-			headers: {
-				authorization: process.env.FISHFISH,
-				"Content-Type": "application/json",
-			},
-		});
-		const data = await response.json();
+		const reportReason = [
+			`Reported by \`${modal.user.username}#${modal.user.discriminator}\` via the Anti.fish API server.`,
+			`Report reason: ${reason}`,
+			`\nAdditional details: ${extra}`,
+		].join("\n");
+
+		const { data } = await axios
+			.post(
+				`https://${config.submitReportDomain}/phish/report`,
+				{
+					url: domain,
+					reason: reportReason,
+				},
+				{
+					headers: {
+						authorization: config.submitReportAuthorization,
+						"Content-Type": "application/json",
+					},
+				},
+			)
+			.catch((error) => {
+				if (error.isAxiosError) {
+					printError(
+						`[status: ${error.response?.status}]: ${JSON.stringify(error.response?.data ?? {})}`,
+					);
+				} else {
+					printError(error);
+				}
+
+				return {
+					data: {
+						accepted: false,
+						reason: error.response?.data?.reason ?? "Unknown reason",
+					},
+				};
+			});
 
 		if (data.accepted) {
-			let embed = new MessageEmbed()
+			const embed = new MessageEmbed()
 				.setDescription(`Successfully reported **${domain}** to fishfish.gg!`)
 				.setFooter({ text: `Reported by ${modal.user.username}#${modal.user.discriminator}` })
 				.setColor("03962a");
-			let row = new MessageActionRow()
+
+			const actionRow = new MessageActionRow()
 				.addComponents(
 					new MessageButton()
 						.setCustomId("submitReport")
@@ -34,6 +61,7 @@ module.exports = {
 						.setStyle("SUCCESS")
 						.setDisabled(true),
 				)
+
 				.addComponents(
 					new MessageButton()
 						.setCustomId("cancelReport")
@@ -41,20 +69,27 @@ module.exports = {
 						.setStyle("DANGER")
 						.setDisabled(true),
 				);
+
 			await modal.reply({
 				content: `Successfully reported **${domain}** to fishfish.gg!`,
 				ephemeral: true,
 			});
-			await modal.message.edit({ embeds: [embed], components: [row] });
-		} else {
-			let embed = new MessageEmbed()
-				.setDescription(`An error occured whilst reporting **${domain}**!\n**"${data.reason}"**`)
-				.setColor("ad071a");
-			await modal.reply({
-				content: `An error occured whilst reporting **${domain}**.\n**${data.reason}**`,
-				ephemeral: true,
+
+			return modal.message.edit({
+				embeds: [embed],
+				components: [actionRow],
 			});
-			await modal.message.edit({ embeds: [embed], components: [] });
 		}
+
+		const embed = new MessageEmbed()
+			.setDescription(`An error occured whilst reporting **${domain}**!\n**"${data.reason}"**`)
+			.setColor("ad071a");
+
+		await modal.reply({
+			content: `An error occured whilst reporting **${domain}**.\n**${data.reason}**`,
+			ephemeral: true,
+		});
+
+		return modal.message.edit({ embeds: [embed], components: [] });
 	},
 };
