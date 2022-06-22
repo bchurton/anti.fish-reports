01a0bd86de99fdb048b14f81382dc301093c1042
T3NED <software@t3ned.com>
2022-06-22 01:39:32 +0100

    Refactor repo command

--- src/commands/repo.js
+++ src/commands/repo.js
@@ -1,18 +1,18 @@
 const { MessageEmbed } = require("discord.js");
+const randomColor = require("../utils/randomColor");
+const config = require("../config");
 
 module.exports = {
 	name: "repo",
 	usage: "repo",
 	description: "View the bot's GitHub repository!",
-	async execute(client, message, args) {
-		console.log("recieved");
-		try {
-			let embed = new MessageEmbed()
-				.setDescription(`You can view my GitHub repo [here](${process.env.repo})!`)
-				.setColor(Math.floor(Math.random() * 16777215).toString(16));
-			await message.channel.send({ embeds: [embed] });
-		} catch (err) {
-			return message.channel.send(`\`\`\`\n${err}\`\`\``); //for development debugging
-		}
+	async execute(_client, message, args) {
+		const embed = new MessageEmbed()
+			.setDescription(`You can view my GitHub repo [here](${config.repositoryUrl} "View the code")!`)
+			.setColor(randomColor());
+
+		return message.channel.send({
+			embeds: [embed],
+		});
 	},
 };
--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const discordToken = process.env.DISCORD_TOKEN;
 const commandPrefix = process.env.COMMAND_PREFIX;
 const debug = process.env.DEBUG === "true";
+const repositoryUrl = process.env.REPOSITORY_URL;
 
 module.exports = {
 	discordToken,
 	commandPrefix,
 	debug,
+	repositoryUrl,
 };
