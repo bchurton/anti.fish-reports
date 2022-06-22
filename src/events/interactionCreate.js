76e9466df914c319aa74887f23e97ad1b72752d0
T3NED <software@t3ned.com>
2022-06-22 01:55:33 +0100

    Refactor interaction create event

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,74 +1,84 @@
-const { Modal, TextInputComponent, SelectMenuComponent, showModal } = require("discord-modals"); // Import all
+const { Modal, TextInputComponent, SelectMenuComponent, showModal } = require("discord-modals");
+
+const handleSubmitReport = (client, interaction, submitReportUserId) => {
+	const interactionUserId = interaction.user.id;
+
+	if (interactionUserId !== submitReportUserId) {
+		return interaction.reply({
+			content: `Only the message author can submit this report!`,
+			ephemeral: true,
+		});
+	}
+
+	const modal = new Modal()
+		.setCustomId(`domainReport`)
+		.setTitle("Submit Domain Report")
+		.addComponents(
+			new TextInputComponent()
+				.setCustomId("domain")
+				.setLabel("Domain")
+				.setStyle("SHORT")
+				.setRequired(true),
+
+			new SelectMenuComponent() //
+				.setCustomId("reason")
+				.setPlaceholder("Domain report reason")
+				.addOptions(
+					{
+						label: "Phishing Link",
+						description: "Sites that attempt to steal personal details",
+						value: "PHISHING",
+						emoji: "🎣",
+					},
+					{
+						label: "Malware",
+						description: "Sites that contain malware",
+						value: "MALWARE",
+						emoji: "🦠",
+					},
+					{
+						label: "Other",
+						description: "If the option you need is not listed, choose this.",
+						value: "OTHER",
+						emoji: "❓",
+					},
+				),
+
+			new TextInputComponent()
+				.setCustomId("extra")
+				.setLabel("Additional Information")
+				.setPlaceholder("Put anything else which may be helpful here!")
+				.setStyle("LONG"),
+		);
+
+	return showModal(modal, {
+		client,
+		interaction,
+	});
+};
+
+const handleCancelReport = (interaction, cancelReportUserId) => {
+	const interactionUserId = interaction.user.id;
+
+	if (interactionUserId !== cancelReportUserId) {
+		return interaction.reply({
+			content: `Only the message author can submit this report!`,
+			ephemeral: true,
+		});
+	}
+
+	return interaction.message.deletable && interaction.message.delete();
+};
 
 module.exports = {
 	name: "interactionCreate",
 	async execute(client, interaction) {
-		if (interaction.customId.includes("submitReport")) {
-			try {
-				const userid = interaction.customId.replace("submitReport", "").split("|")[0];
-				if (interaction.user.id !== userid) {
-					return await interaction.reply({
-						content: `Only the message author can submit this report!`,
-						ephemeral: true,
-					});
-				}
+		if (!interaction.isButton()) return;
 
-				const modal = new Modal()
-					.setCustomId(`domainReport`)
-					.setTitle("Submit Domain Report")
-					.addComponents(
-						new TextInputComponent()
-							.setCustomId("domain")
-							.setLabel("Domain")
-							.setStyle("SHORT")
-							.setRequired(true),
+		const submitReportUserId = interaction.customId.match(/(^submitReport)(\d+)/)?.[2];
+		if (submitReportUserId) return handleSubmitReport(client, interaction, submitReportUserId);
 
-						new SelectMenuComponent()
-							.setCustomId("reason")
-							.setPlaceholder("Domain report reason")
-							.addOptions(
-								{
-									label: "Phishing Link",
-									description: "Sites that attempt to steal personal details",
-									value: "PHISHING",
-									emoji: "🎣",
-								},
-								{
-									label: "Malware",
-									description: "Sites that contain malware",
-									value: "MALWARE",
-									emoji: "🦠",
-								},
-								{
-									label: "Other",
-									description: "If the option you need is not listed, choose this.",
-									value: "OTHER",
-									emoji: "❓",
-								},
-							),
-						new TextInputComponent()
-							.setCustomId("extra")
-							.setLabel("Additional Information")
-							.setPlaceholder("Put anything else which may be helpful here!")
-							.setStyle("LONG"),
-					);
-				showModal(modal, {
-					client: client,
-					interaction: interaction,
-				});
-			} catch (err) {
-				console.log(`Error: ${err}`);
-			}
-		} else if (interaction.customId.includes("cancelReport")) {
-			const toSplit = interaction.customId.replace("cancelReport", "").split("|");
-			const userid = toSplit[0];
-			if (interaction.user.id !== userid) {
-				return await interaction.reply({
-					content: `Only the message author can submit this report!`,
-					ephemeral: true,
-				});
-			}
-			await interaction.message.delete();
-		}
+		const cancelReportUserId = interaction.customId.match(/(^cancelReport)(\d+)/)?.[2];
+		if (cancelReportUserId) return handleCancelReport(interaction, cancelReportUserId);
 	},
 };
