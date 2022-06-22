eb30c0ea5f0348c59971379040240ee5a3957797
T3NED <software@t3ned.com>
2022-06-22 01:08:54 +0100

    Add debugging to handlers

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,15 +1,19 @@
+const { printDebug, printSuccess } = require("../utils/console");
 const { walkFiles, handleFromFilePaths } = require("./handler");
 
 const loadCommands = (client, commandDir) => {
+	printDebug("Loading commands...");
+
 	const filePaths = walkFiles(commandDir);
 
 	const handle = (command) => {
 		client.commands.set(command.name, command.execute);
+		printSuccess(`Loaded command '${command.name}'`);
 	};
 
 	handleFromFilePaths(filePaths, handle);
+
+	printSuccess("Loaded commands");
 };
 
-module.exports = {
-	loadCommands,
-};
+module.exports = loadCommands;
--- src/handlers/event.js
+++ src/handlers/event.js
@@ -1,15 +1,19 @@
+const { printDebug, printSuccess } = require("../utils/console");
 const { walkFiles, handleFromFilePaths } = require("./handler");
 
 const loadEvents = (client, eventDir) => {
+	printDebug("Loading events...");
+
 	const filePaths = walkFiles(eventDir);
 
 	const handle = (event) => {
 		client.on(event.name, (...args) => event.execute(client, ...args));
+		printSuccess(`Loaded event '${event.name}'`);
 	};
 
 	handleFromFilePaths(filePaths, handle);
+
+	printSuccess("Loaded events");
 };
 
-module.exports = {
-	loadEvents,
-};
+module.exports = loadEvents;
