c4d11311a39520bb3f6868dfd968ac2bf05e81bb
T3NED <software@t3ned.com>
2022-06-22 01:10:24 +0100

    Add debug to config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 const discordToken = process.env.DISCORD_TOKEN;
 const commandPrefix = process.env.COMMAND_PREFIX;
+const debug = process.env.DEBUG === "true";
 
 module.exports = {
 	discordToken,
 	commandPrefix,
+	debug,
 };
